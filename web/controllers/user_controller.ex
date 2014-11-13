c03d4645c37489a4235f76feae3029b1f7022a35
Anton Fagerberg <anton.fagerberg@jayway.com>
2014-11-13 21:24:24 +0100

    Removed comments

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -58,45 +58,4 @@ defmodule ComicBaker.UserController do
     conn = delete_session(conn, :email)
     redirect conn, "/"
   end
-  
-  #def upload_test(conn, _params) do
-  #  conn
-  #  |> put_resp_header("content-type", Plug.MIME.path("image/png"))
-  #  |> send_file(200, "/Users/anton/Documents/EarthBound\ at\ 22.02.06.png")
-    #|> halt
-    #render conn, "upload_test"
-  #end
-  
-  #def index(conn, _params) do
-    #IO.puts Repo.all(User)
-    #users = Repo.all(User)
-    #List.size(users)
-    #{:ok, key} = :pbkdf2.pbkdf2(:sha512, "password", "salt", 20480, 160)
-
-  #  render conn, "list", users: Repo.all(User) #"index", users: Repo.all(User)
-  #end
-
-  #def show(conn, %{"id" => id}) do
-  #  case Repo.get(User, id) do
-  #    user when is_map(user) -> render conn, "Showing user #{user}" #"show", user: user
-  #    _ -> redirect conn, Router.page_path(page: "unauthorized")
-  #  end
-  #end
-
-  #def new(conn, _params) do
-  #  IO.puts "NEW!!!"
-  #  render conn, "new"
-  #end
-
-  #def create(conn, %{"user" => %{"content" => content}}) do
-  #  user = %User{content: content}
-#
-#    case User.validate(user) do
-#      [] ->
-#        user = Repo.insert(user)
-#        render conn, "show", user: user
-#      errors ->
-#        render conn, "new", user: user, errors: errors
-#    end
-#  end
 end
